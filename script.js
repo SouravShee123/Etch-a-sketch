683258fe80f6c58bbe62986f845efd4758ea8577
Sourav <313756942+SouravShee123@users.noreply.github.com>
2026-09-09 01:06:02 +0530

    Added button to take the length from user

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 const container = document.querySelector(".grid-container");
+const button = document.querySelector(".btn");
 
-for(let i=0; i<256; i++){
-    const square = document.createElement("div");
-    square.classList.add("grid-box"); // adds grid-box as the class for square
+// function to genarate grid 
+function createGrid(size) {
 
-    /* 'mouseenter' applies style permanently unlike ':hover' */
+    container.innerHTML = ""; // clears out the old grid completely
 
-    square.addEventListener("mouseenter",()=>{
-        square.style.backgroundColor = "purple"
-    })
-    container.appendChild(square); 
+    const totalSquare = size * size;
+    const sizePercentage = `${100 / size}%`; // height/width percentage for each square
+
+    for(let i=0; i<totalSquare; i++){
+        const square = document.createElement("div");
+            square.classList.add("grid-box");
+            
+            //set dynamic sizing directly
+            square.style.height = sizePercentage;
+            square.style.width = sizePercentage;
+
+            // handles trailing effect
+            square.addEventListener("mouseenter",()=>{
+                square.style.backgroundColor = "black"
+            })
+            container.appendChild(square); 
+    }
 }
 
+createGrid(16); // creating 16x16 grid initially
+
+// adding button functions
+button.addEventListener("click",()=>{
+    const sideLength=parseInt(prompt("Enter the length of one side(max 100)"));
+
+    if(isNaN(sideLength) || sideLength <= 0){
+        alert("Please enter a positive number");
+    }
+    else if(sideLength > 100){
+        alert("Maximun limit exceeded ! Please enter a lower number");
+    }
+    else{
+        createGrid(sideLength);
+    }
+})
