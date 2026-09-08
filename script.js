da47fab66ef6f5021323d00638b82f93fb50ee9f
Sourav <313756942+SouravShee123@users.noreply.github.com>
2026-09-08 20:51:36 +0530

    Added EventListener for trailing effect
    Used mouseenter for permanent effect not :hover

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ const container = document.querySelector(".grid-container");
 for(let i=0; i<256; i++){
     const square = document.createElement("div");
     square.classList.add("grid-box"); // adds grid-box as the class for square
+
+    /* 'mouseenter' applies style permanently unlike ':hover' */
+
+    square.addEventListener("mouseenter",()=>{
+        square.style.backgroundColor = "purple"
+    })
     container.appendChild(square); 
 }
+
